d33c714736b867f5e95ce94d760930e1c64fa895
sbhbenjamin <sbhbenjamin@gmail.com>
2022-02-27 19:27:11 +0800

    Add task class, task properties and task exceptions

---
+++ src/main/java/seedu/address/model/task/Deadline.java
@@ -0,0 +1,55 @@
+package seedu.address.model.task;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Task's deadline in the task list.
+ * Guarantees: immutable; is valid as declared in {@link #isValidDeadline(String)}
+ */
+public class Deadline {
+
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Deadlines should be in the format YYYY-MM-DD";
+    // regex adapted from:
+    // https://www.regular-expressions.info/dates.html#:~:text=To%20match%20a%20date%20in,%5D)%5B%2D%20%2F.%5D
+    public static final String VALIDATION_REGEX = "^(19|20)\\d\\d([- /.])(0[1-9]|1[012])\\2(0[1-9]|[12][0-9]|3[01])$";
+    public final String value;
+
+    /**
+     * Constructs a {@code Deadline}.
+     *
+     * @param deadline A valid deadline.
+     */
+    public Deadline(String deadline) {
+        requireNonNull(deadline);
+        checkArgument(isValidDeadline(deadline), MESSAGE_CONSTRAINTS);
+        value = deadline;
+    }
+
+    /**
+     * Returns true if a given string is a valid deadline.
+     */
+    public static boolean isValidDeadline(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Deadline // instanceof handles nulls
+                && value.equals(((Deadline) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
---
+++ src/main/java/seedu/address/model/task/Description.java
@@ -0,0 +1,39 @@
+package seedu.address.model.task;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a Task's description in the task list.
+ * Guarantees: immutable;
+ */
+public class Description {
+
+    public final String value;
+
+    /**
+     * Constructs a {@code Description}.
+     *
+     * @param description A valid description.
+     */
+    public Description(String description) {
+        requireNonNull(description);
+        value = description;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Description // instanceof handles nulls
+                && value.equals(((Description) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
