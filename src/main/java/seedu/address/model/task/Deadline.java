3861b025e6e8eff9a927094586aa425d33f355a9
sbhbenjamin <sbhbenjamin@gmail.com>
2022-03-06 13:17:53 +0800

    Update deadline validation and store deadline as LocalDate

--- src/main/java/seedu/address/model/task/Deadline.java
+++ src/main/java/seedu/address/model/task/Deadline.java
@@ -3,41 +3,45 @@ package seedu.address.model.task;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
 /**
  * Represents a Task's deadline in the task list.
  * Guarantees: immutable; is valid as declared in {@link #isValidDeadline(String)}
  */
 public class Deadline {
 
-
     public static final String MESSAGE_CONSTRAINTS =
             "Deadlines should be in the format YYYY-MM-DD";
-    // regex adapted from:
-    // https://www.regular-expressions.info/dates.html#:~:text=To%20match%20a%20date%20in,%5D)%5B%2D%20%2F.%5D
-    public static final String VALIDATION_REGEX = "^(19|20)\\d\\d([- /.])(0[1-9]|1[012])\\2(0[1-9]|[12][0-9]|3[01])$";
-    public final String value;
+    public final LocalDate value;
 
     /**
-     * Constructs a {@code Deadline}.
+     * Constructs a {@code Deadline}. Accepts date in the format of yyyy-mm-dd.
      *
      * @param deadline A valid deadline.
      */
     public Deadline(String deadline) {
         requireNonNull(deadline);
         checkArgument(isValidDeadline(deadline), MESSAGE_CONSTRAINTS);
-        value = deadline;
+        value = LocalDate.parse(deadline);
     }
 
     /**
      * Returns true if a given string is a valid deadline.
      */
     public static boolean isValidDeadline(String test) {
-        return test.matches(VALIDATION_REGEX);
+        try {
+            LocalDate.parse(test);
+            return true;
+        } catch (DateTimeParseException error) {
+            return false;
+        }
     }
 
     @Override
     public String toString() {
-        return value;
+        return value.toString();
     }
 
     @Override
