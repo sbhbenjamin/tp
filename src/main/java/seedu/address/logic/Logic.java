e91f33744b5691cefd69471b78644719ec03448f
joeietao <77208712+joeietao@users.noreply.github.com>
2022-03-09 11:50:54 +0800

    Update Ui, Logic, Logic Manager and AddressBookParser
    1. Updated all usage of person and address book to become task and task list
    2. Changed name of AddressBookParser to TaskListParser for use in LogicManager

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -8,7 +8,9 @@ import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
+import seedu.address.model.ReadOnlyTaskList;
 import seedu.address.model.person.Person;
+import seedu.address.model.task.Task;
 
 /**
  * API of the Logic component
@@ -26,17 +28,17 @@ public interface Logic {
     /**
      * Returns the AddressBook.
      *
-     * @see seedu.address.model.Model#getAddressBook()
+     * @see seedu.address.model.Model#getTaskList()
      */
-    ReadOnlyAddressBook getAddressBook();
+    ReadOnlyTaskList getTaskList();
 
     /** Returns an unmodifiable view of the filtered list of persons */
-    ObservableList<Person> getFilteredPersonList();
+    ObservableList<Task> getFilteredTaskList();
 
     /**
      * Returns the user prefs' address book file path.
      */
-    Path getAddressBookFilePath();
+    Path getTaskListFilePath();
 
     /**
      * Returns the user prefs' GUI settings.
