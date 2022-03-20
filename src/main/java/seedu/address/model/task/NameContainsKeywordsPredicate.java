a6dd011ab20c84ea39ff96d0ace24775befca125
sbhbenjamin <sbhbenjamin@gmail.com>
2022-03-20 17:52:20 +0800

    Add tests for Task and its properties

--- src/main/java/seedu/address/model/task/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/task/NameContainsKeywordsPredicate.java
@@ -23,7 +23,7 @@ public class NameContainsKeywordsPredicate implements Predicate<Task> {
 
     @Override
     public boolean equals(Object other) {
-        return other == this // short circuit if same object
+        return other == this // short circuit if same objectz
                 || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
                 && keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
     }
---
+++ src/test/java/seedu/address/model/task/UniqueTaskListTest.java
@@ -0,0 +1,170 @@
+package seedu.address.model.task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DESCRIPTION_MIDTERM;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_TEST;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTasks.CS2103T_PROJECT;
+import static seedu.address.testutil.TypicalTasks.CS2105_MIDTERM;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.task.exceptions.DuplicateTaskException;
+import seedu.address.model.task.exceptions.TaskNotFoundException;
+import seedu.address.testutil.TaskBuilder;
+
+public class UniqueTaskListTest {
+
+    private final UniqueTaskList uniqueTaskList = new UniqueTaskList();
+
+    @Test
+    public void contains_nullTask_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.contains(null));
+    }
+
+    @Test
+    public void contains_taskNotInList_returnsFalse() {
+        assertFalse(uniqueTaskList.contains(CS2105_MIDTERM));
+    }
+
+    @Test
+    public void contains_taskInList_returnsTrue() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        assertTrue(uniqueTaskList.contains(CS2105_MIDTERM));
+    }
+
+    @Test
+    public void contains_taskWithSameIdentityFieldsInList_returnsTrue() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        Task editedAlice = new TaskBuilder(CS2105_MIDTERM).withDescription(VALID_DESCRIPTION_MIDTERM).withTags(VALID_TAG_TEST)
+                .build();
+        assertTrue(uniqueTaskList.contains(editedAlice));
+    }
+
+    @Test
+    public void add_nullTask_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.add(null));
+    }
+
+    @Test
+    public void add_duplicateTask_throwsDuplicateTaskException() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        assertThrows(DuplicateTaskException.class, () -> uniqueTaskList.add(CS2105_MIDTERM));
+    }
+
+    @Test
+    public void setTask_nullTargetTask_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.setTask(null, CS2105_MIDTERM));
+    }
+
+    @Test
+    public void setTask_nullEditedTask_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.setTask(CS2105_MIDTERM, null));
+    }
+
+    @Test
+    public void setTask_targetTaskNotInList_throwsTaskNotFoundException() {
+        assertThrows(TaskNotFoundException.class, () -> uniqueTaskList.setTask(CS2105_MIDTERM, CS2105_MIDTERM));
+    }
+
+    @Test
+    public void setTask_editedTaskIsSameTask_success() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        uniqueTaskList.setTask(CS2105_MIDTERM, CS2105_MIDTERM);
+        UniqueTaskList expectedUniqueTaskList = new UniqueTaskList();
+        expectedUniqueTaskList.add(CS2105_MIDTERM);
+        assertEquals(expectedUniqueTaskList, uniqueTaskList);
+    }
+
+    @Test
+    public void setTask_editedTaskHasSameIdentity_success() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        Task editedAlice = new TaskBuilder(CS2105_MIDTERM).withDescription(VALID_DESCRIPTION_MIDTERM).withTags(VALID_TAG_TEST)
+                .build();
+        uniqueTaskList.setTask(CS2105_MIDTERM, editedAlice);
+        UniqueTaskList expectedUniqueTaskList = new UniqueTaskList();
+        expectedUniqueTaskList.add(editedAlice);
+        assertEquals(expectedUniqueTaskList, uniqueTaskList);
+    }
+
+    @Test
+    public void setTask_editedTaskHasDifferentIdentity_success() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        uniqueTaskList.setTask(CS2105_MIDTERM, CS2103T_PROJECT);
+        UniqueTaskList expectedUniqueTaskList = new UniqueTaskList();
+        expectedUniqueTaskList.add(CS2103T_PROJECT);
+        assertEquals(expectedUniqueTaskList, uniqueTaskList);
+    }
+
+    @Test
+    public void setTask_editedTaskHasNonUniqueIdentity_throwsDuplicateTaskException() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        uniqueTaskList.add(CS2103T_PROJECT);
+        assertThrows(DuplicateTaskException.class, () -> uniqueTaskList.setTask(CS2105_MIDTERM, CS2103T_PROJECT));
+    }
+
+    @Test
+    public void remove_nullTask_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.remove(null));
+    }
+
+    @Test
+    public void remove_taskDoesNotExist_throwsTaskNotFoundException() {
+        assertThrows(TaskNotFoundException.class, () -> uniqueTaskList.remove(CS2105_MIDTERM));
+    }
+
+    @Test
+    public void remove_existingTask_removesTask() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        uniqueTaskList.remove(CS2105_MIDTERM);
+        UniqueTaskList expectedUniqueTaskList = new UniqueTaskList();
+        assertEquals(expectedUniqueTaskList, uniqueTaskList);
+    }
+
+    @Test
+    public void setTasks_nullUniqueTaskList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.setTasks((UniqueTaskList) null));
+    }
+
+    @Test
+    public void setTasks_uniqueTaskList_replacesOwnListWithProvidedUniqueTaskList() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        UniqueTaskList expectedUniqueTaskList = new UniqueTaskList();
+        expectedUniqueTaskList.add(CS2103T_PROJECT);
+        uniqueTaskList.setTasks(expectedUniqueTaskList);
+        assertEquals(expectedUniqueTaskList, uniqueTaskList);
+    }
+
+    @Test
+    public void setTasks_nullList_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueTaskList.setTasks((List<Task>) null));
+    }
+
+    @Test
+    public void setTasks_list_replacesOwnListWithProvidedList() {
+        uniqueTaskList.add(CS2105_MIDTERM);
+        List<Task> taskList = Collections.singletonList(CS2103T_PROJECT);
+        uniqueTaskList.setTasks(taskList);
+        UniqueTaskList expectedUniqueTaskList = new UniqueTaskList();
+        expectedUniqueTaskList.add(CS2103T_PROJECT);
+        assertEquals(expectedUniqueTaskList, uniqueTaskList);
+    }
+
+    @Test
+    public void setTasks_listWithDuplicateTasks_throwsDuplicateTaskException() {
+        List<Task> listWithDuplicateTasks = Arrays.asList(CS2105_MIDTERM, CS2105_MIDTERM);
+        assertThrows(DuplicateTaskException.class, () -> uniqueTaskList.setTasks(listWithDuplicateTasks));
+    }
+
+    @Test
+    public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, ()
+            -> uniqueTaskList.asUnmodifiableObservableList().remove(0));
+    }
+}
