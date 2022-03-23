8fd6e35fafba9c0fb9296360e65ed81b72afdba3
sbhbenjamin <sbhbenjamin@gmail.com>
2022-03-23 11:54:11 +0800

    Update storage to support priority field

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -11,6 +11,7 @@ import seedu.address.model.task.CompletionStatus;
 import seedu.address.model.task.Deadline;
 import seedu.address.model.task.Description;
 import seedu.address.model.task.Name;
+import seedu.address.model.task.Priority;
 import seedu.address.model.task.Task;
 
 /**
@@ -20,18 +21,18 @@ public class SampleDataUtil {
     public static Task[] getSampleTasks() {
         return new Task[] {
             new Task(new Name("Complete Tutorial"), new Description("Hand draw UML diagrams"),
-                new CompletionStatus("true"), new Deadline("2022-03-10"), getTagSet("CS2103T")),
+                new CompletionStatus("true"), new Deadline("2022-03-10"), Priority.LOW, getTagSet("CS2103T")),
             new Task(new Name("Prepare for Presentation"), new Description("Finalise slides and memorise script"),
-                new CompletionStatus("false"), new Deadline("2022-03-12"), getTagSet("CS2101")),
+                new CompletionStatus("false"), new Deadline("2022-03-12"), Priority.LOW, getTagSet("CS2101")),
             new Task(new Name("Review individual projects"),
                 new Description("Review two ips that are sent to my email, for 4 participation points"),
-                new CompletionStatus("true"), new Deadline("2022-03-13"),
+                new CompletionStatus("true"), new Deadline("2022-03-13"), Priority.LOW,
                 getTagSet("CS2103T", "participation")),
             new Task(new Name("Revise for Midterm Test"), new Description("Chapters 1-5"),
-                new CompletionStatus("false"), new Deadline("2022-03-25"), getTagSet("CS2102")),
+                new CompletionStatus("false"), new Deadline("2022-03-25"), Priority.LOW, getTagSet("CS2102")),
             new Task(new Name("Complete Assignment"),
                 new Description("Name submission file as assignment1_name and upload to Luminus folder"),
-                new CompletionStatus("false"), new Deadline("2022-04-01"), getTagSet("CS2105"))
+                new CompletionStatus("false"), new Deadline("2022-04-01"), Priority.LOW, getTagSet("CS2105"))
         };
     }
 
