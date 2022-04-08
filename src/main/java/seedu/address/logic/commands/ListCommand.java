50e9d35c0f4c01a11236c89b66e52bbec23b95cf
thikhinab <thikhinab@gmail.com>
2022-04-08 17:56:02 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TASKS;
 
 import java.util.Set;
 import java.util.StringJoiner;
